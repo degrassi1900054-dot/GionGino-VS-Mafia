b40f025c26503ea501ccf80a69c333e4461bed9b
MinSus03 <138965028+MinSus03@users.noreply.github.com>
2026-05-26 13:57:22 +0200

    Add files via upload
    done: le mappe appaiono su schermo to do: le mappe si muovono con il movimento della telecamera, rendendo raggiungerle col player impossibile, inoltre non hanno collisioni

---
+++ JohnGino/maps/Concrete_Tileset_64x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Concrete_Tileset_64x" tilewidth="64" tileheight="64" tilecount="24" columns="6">
+ <image source="asset_grafici/Concrete_Tileset_64x.png" width="384" height="256"/>
+</tileset>
